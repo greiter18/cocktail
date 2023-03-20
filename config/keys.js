dfa0b4fef05530bc0f8a8a36dfa8df6d5f2914d2
gabriel <gabeh.reiter@gmail.com>
2023-03-20 16:46:27 -0400

    gitignore changes

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,1 @@
 // iahjL5Z2fLqObs2T
-// mongodb+srv://greiter:iahjL5Z2fLqObs2T@mern.hlmzcf4.mongodb.net/?retryWrites=true&w=majority
-
-export const MONGO_URI = {
-  mongo_uri: "mongodb+srv://greiter:iahjL5Z2fLqObs2T@mern.hlmzcf4.mongodb.net/?retryWrites=true&w=majority"
-}
