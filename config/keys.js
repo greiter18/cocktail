fae7d919885ddf83ee51a079b8b2d261e223086e
gabriel <gabeh.reiter@gmail.com>
2023-03-20 16:34:44 -0400

    mongoose connected

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,6 @@
-// password: iahjL5Z2fLqObs2T
-
+// iahjL5Z2fLqObs2T
 // mongodb+srv://greiter:iahjL5Z2fLqObs2T@mern.hlmzcf4.mongodb.net/?retryWrites=true&w=majority
+
+export const MONGO_URI = {
+  mongo_uri: "mongodb+srv://greiter:iahjL5Z2fLqObs2T@mern.hlmzcf4.mongodb.net/?retryWrites=true&w=majority"
+}
--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import {MONGO_URI} from "./config/keys.js";
+const mongo_uri = MONGO_URI.mongo_uri;
+import bodyParser from "body-parser";
 
 const app = express();
+const router = express.Router();
+
+mongoose
+  .connect(mongo_uri, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
