ecdd05accaf5bbb5e4704a340b1486583f7f89fe
gabriel <gabeh.reiter@gmail.com>
2023-03-23 14:22:31 -0400

    login function added

--- routes/api/users.js
+++ routes/api/users.js
@@ -33,4 +33,24 @@ router.post('/register', (req, res) => {
   })
 })
 
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email})
+    .then(user => {
+      if(!user){
+        return res.status(404).json({email: "This User does not exist"});
+      }
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch){
+            res.json({msg: "Success! "});
+          } else {
+            return res.status(400).json({password: "Password is incorrect"});
+          }
+        })
+    })
+})
+
 export const users = router;
