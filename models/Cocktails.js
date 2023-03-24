667ceb02e0893850938849efccf5a3551c2db15a
gabriel <gabeh.reiter@gmail.com>
2023-03-23 23:59:12 -0400

    Cocktail Schema added

---
+++ models/Cocktails.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const CocktailSchema = new Schema({
+  user_id: {type:  Schema.Types.ObjectId,ref: 'users'},
+  name: {type: String, required: true},
+  ingredients: {type: String, required: true},
+  image: {type: String, required: true},
+  instructions: {type: String, required: true},
+  glass: {type: String, required: true}
+
+})
+
+export const Cocktail = mongoose.model("Cocktail", CocktailSchema);
