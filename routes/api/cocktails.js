bca36dbc560f4274303e1fc94fa79b98b46dac60
gabriel <gabeh.reiter@gmail.com>
2023-03-29 07:52:10 -0400

    Frontend files created

--- routes/api/cocktails.js
+++ routes/api/cocktails.js
@@ -8,6 +8,32 @@ router.get("/test",(req, res) => {
   res.json({msg: 'This is the cocktail route'});
 })
 
+router.get("/",(req, res) => {
+  Cocktail
+  .find()
+  .sort({date: -1})
+  .then(cocktail => res.json(cocktail))
+  .catch(err => res.status(400).json(err))
+})
+
+router.get("/user/:user_id",
+  passport.authenticate("jwt", {session: false}),
+  (req, res) => {
+    Cocktail
+      .find({user_id: req.params.user_id})
+      .then(cocktail => res.json(cocktail))
+      .catch(err => res.status(400).json(err));
+  }
+)
+
+router.get("/:id", (req,res) => {
+  Cocktail
+    .findById(req.params.id)
+    .then(cocktail => res.json(cocktail))
+    .catch(err => res.status(400).json(err))
+  }
+);
+
 router.post("/", 
   passport.authenticate("jwt", {session:false}),
   (req,res) => {
@@ -28,5 +54,13 @@ router.post("/",
   }
 )
 
+router.delete("/:cocktail_id", 
+  passport.authenticate("jwt", {session: false}),
+  (req,res) => {
+    Cocktail.findByIdAndRemove(req.params.cocktail_id)
+    .then((cocktails) => res.json(cocktails))
+    .catch(err => res.status(400).json(err));
+  }
+)
 
 export const cocktails = router;
